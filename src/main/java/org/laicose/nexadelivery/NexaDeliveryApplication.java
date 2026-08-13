00bdb5c9f702862a054f2349b0b49dcd0b978f03
zakaria outla <zakariadevlop78@gmail.com>
2026-08-13 11:56:27 +0100

    create projet backend java spring boot

---
+++ src/main/java/org/laicose/nexadelivery/NexaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package org.laicose.nexadelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexaDeliveryApplication.class, args);
+    }
+
+}
